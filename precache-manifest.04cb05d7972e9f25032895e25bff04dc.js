07b24eef83735d9f58bb490960bc165cbb94a873
Bartoszkie <bartosz.slysz23@gmail.com>
2019-11-03 21:22:45 +0100

    Updates

---
+++ precache-manifest.04cb05d7972e9f25032895e25bff04dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d77c77b50509cfd170a20fbe9f6b3d",
+    "url": "/Sample-SPA/index.html"
+  },
+  {
+    "revision": "e4419507e98743f1dfda",
+    "url": "/Sample-SPA/static/css/main.5c71a53a.chunk.css"
+  },
+  {
+    "revision": "1aeb4ac31fbc0891164f",
+    "url": "/Sample-SPA/static/js/2.1406e7a4.chunk.js"
+  },
+  {
+    "revision": "e4419507e98743f1dfda",
+    "url": "/Sample-SPA/static/js/main.dd456e28.chunk.js"
+  },
+  {
+    "revision": "c215aa8b59448190de8f",
+    "url": "/Sample-SPA/static/js/runtime-main.a71be068.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Sample-SPA/precache-manifest.92edd924f0612cca5af9aa7f5895084e.js"
+  "/Sample-SPA/precache-manifest.04cb05d7972e9f25032895e25bff04dc.js"
 );
 
 self.addEventListener('message', (event) => {
